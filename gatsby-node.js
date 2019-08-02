97e6b0d7d37de46480f44073456bd010b71288f4
Skye Soss <skyler.soss@gmail.com>
2019-08-01 21:56:34 -0400

    Added "@" webpack alias

---
+++ gatsby-node.js
@@ -0,0 +1,11 @@
+// Allow `import '@/components/header.js'`
+const path = require('path')
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, 'src')
+      }
+    },
+  })
+}
